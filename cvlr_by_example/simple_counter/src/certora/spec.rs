9bb713b382ef320fd2c66c46ae84a44368801af0
MonokumaLemon <158763505+MonokumaLemon@users.noreply.github.com>
2025-06-28 23:08:37 +0200

    Implement certora spec files for simple_counter

---
+++ cvlr_by_example/simple_counter/src/certora/spec.rs
@@ -0,0 +1,37 @@
+use crate::{processor::*, state::SimpleCounter};
+use cvlr::{mathint::NativeInt, prelude::*};
+use cvlr_solana::cvlr_deserialize_nondet_accounts;
+use solana_program::account_info::{next_account_info, AccountInfo};
+
+struct FvSimpleCounter {
+    ctr: NativeInt,
+}
+
+impl From<&SimpleCounter> for FvSimpleCounter {
+    fn from(counter: &SimpleCounter) -> FvSimpleCounter {
+        let ctr: u64 = counter.ctr.into();
+        FvVault {
+            ctr: ctr.into(),
+        }
+    }
+}
+
+impl<'a> From<&AccountInfo<'a>> for FvSimpleCounter {
+    fn from(acc_info: &AccountInfo) -> FvSimpleCounter {
+        let mut data = acc_info.data.borrow_mut();
+        let counter: &FvSimpleCounter = bytemuck::from_bytes_mut(&mut data[..]);
+        FvSimpleCounter::from(counter)
+    }
+}
+
+#[rule]
+pub fn rule_correct_increment() {
+    let account_infos = cvlr_deserialize_nondet_accounts();
+    let account_info_iter = &mut account_infos.iter();
+    let simple_counter: &AccountInfo = next_account_info(account_info_iter).unwrap();
+    let fv_counter_pre: FvSimpleCounter = simple_counter.into();
+    cvlr_assume!(fv_counter_pre.ctr < u32::MAX);
+    let program_id = &crate::id();
+    process_start(program_id, &account_infos).unwrap();
+    let fv_counter_post: FvSimpleCounter = simple_counter.into();
+}
